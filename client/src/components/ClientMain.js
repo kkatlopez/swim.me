cc08558d5593a2490987d03eb01f411802c51d0b
Katrina Lopez <lopezk3@rpi.edu>
2022-02-27 13:38:00 -0500

    Update ClientMain.js

--- client/src/components/ClientMain.js
+++ client/src/components/ClientMain.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import FrontPage from './FrontPage.js';
 
+// localhost:3000
+
 const Main = (props) => {
   //console.log(props);
   return (
