f779b6783183723664228bcbd56f442637d1e821
Katrina Lopez <lopezk3@rpi.edu>
2022-02-27 14:35:04 -0500

    testing react bootstrap

--- client/src/components/FrontPage.js
+++ client/src/components/FrontPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 //import { Link, withRouter } from 'react-router-dom';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Alert, Card } from 'react-bootstrap';
 import '../css/frontpage.css';
 
 class FrontPage extends Component {
@@ -15,7 +15,22 @@ class FrontPage extends Component {
     return(
       <Container className="frontpage-main">
         <h1 className="front-title">Welcome!</h1>
+        <Alert variant="success">
+        <Alert.Heading>Hey, nice to see you</Alert.Heading>
+        <p>
+          Aww yeah, you successfully read this important alert message. This example
+          text is going to run a bit longer so that you can see how spacing within an
+          alert works with this kind of content.
+        </p>
+        <hr />
+        <p className="mb-0">
+          Whenever you need to, be sure to use margin utilities to keep things nice
+          and tidy.
+        </p>
+      </Alert>
+      <Card body bg='danger'>This is some text within a card body.</Card>
       </Container>
+      
     );
   }
 }
