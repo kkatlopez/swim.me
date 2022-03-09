1f1a18471e388a791421809f8cadc836abd57345
xianlany <40433521+xianlany@users.noreply.github.com>
2022-03-09 11:52:42 -0500

    added dynamic show/hide for M 500 Free

--- client/src/components/AllTimeTop10.js
+++ client/src/components/AllTimeTop10.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
-import { Container, Card, DropdownButton, Dropdown, Table } from 'react-bootstrap';
+import { Container, DropdownButton, Dropdown} from 'react-bootstrap';
 import '../css/alltimetop10.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import Top10IndividualEvent from './Top10IndividualEvent.js';
 
 class AllTimeTop10 extends Component {
 	
   constructor(props) {
 	super(props);
-		
+  this.state = {
+    event: "",
+    showTable: false
+  };
+  this.showTable = this.showTable.bind(this);
   }
 	
+  showTable(event) {
+    console.log(event);
+    switch (event) {
+      case "M 500 Free":
+        this.setState({ showTable: true });
+        break;
+      case "":
+        this.setState({ showTable: false });
+        break;
+    }
+  }
+
   render() {
+    const { showTable } = this.state;
     return(
       <Container fluid className="page-container">
         <Container fluid className="siteHeader d-flex align-items-end">
@@ -21,6 +39,7 @@ class AllTimeTop10 extends Component {
         <Container className="px-4">
           <label>Event</label>
           <DropdownButton className="dropdown" title="Select an event" className="pb-3">
+            <Dropdown.Item onClick={() => this.showTable("")}>-</Dropdown.Item>
             <Dropdown.Item href="#/action-1">W 50 Free</Dropdown.Item>
             <Dropdown.Item href="#/action-2">M 50 Free</Dropdown.Item>
             <Dropdown.Item href="#/action-3">W 100 Free</Dropdown.Item>
@@ -28,85 +47,13 @@ class AllTimeTop10 extends Component {
             <Dropdown.Item href="#/action-5">W 200 Free</Dropdown.Item>
             <Dropdown.Item href="#/action-6">M 200 Free</Dropdown.Item>
             <Dropdown.Item href="#/action-7">W 500 Free</Dropdown.Item>
-            <Dropdown.Item href="#/action-8">M 500 Free</Dropdown.Item>
+            <Dropdown.Item onClick={() => this.showTable("M 500 Free")}>M 500 Free</Dropdown.Item>
             <Dropdown.Item href="#/action-9">W 1000 Free</Dropdown.Item>
             <Dropdown.Item href="#/action-10">M 1000 Free</Dropdown.Item>
           </DropdownButton>
-          <h2>Latest Results</h2>
-          <div className="event-time">
-          <Table striped bordered hover>
-            <thead>
-                <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Time</th>
-                <th>Year</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>1</td>
-                <td>Trevor Maxfield</td>
-                <td>4:35:95</td>
-                <td>2020</td>
-                </tr>
-                <tr>
-                <td>2</td>
-                <td>Pat Fell</td>
-                <td>4:36:98</td>
-                <td>2004</td>
-                </tr>
-                <tr>
-                <td>3</td>
-                <td>Nathan Hosking</td>
-                <td>4:39:04</td>
-                <td>2014</td>
-                </tr>
-                <tr>
-                <td>4</td>
-                <td>Ben Fell</td>
-                <td>4:39:46</td>
-                <td>2003</td>
-                </tr>
-                <tr>
-                <td>5</td>
-                <td>Dillon Webster</td>
-                <td>4:41:72</td>
-                <td>2012</td>
-                </tr>
-                <tr>
-                <td>6</td>
-                <td>David James</td>
-                <td>4:42:48</td>
-                <td>2014</td>
-                </tr>
-                <tr>
-                <td>7</td>
-                <td>Daniel Hendricks</td>
-                <td>4:42:84</td>
-                <td>2015</td>
-                </tr>
-                <tr>
-                <td>8</td>
-                <td>Keven Ehrlich</td>
-                <td>4:43:47</td>
-                <td>2010</td>
-                </tr>
-                <tr>
-                <td>9</td>
-                <td>Colin Lenhoff</td>
-                <td>4:43:86</td>
-                <td>2012</td>
-                </tr>
-                <tr>
-                <td>10</td>
-                <td>Robert Unruh</td>
-                <td>4:44:37</td>
-                <td>1993</td>
-                </tr>
-            </tbody>
-            </Table>
-          </div>
+
+          {showTable && <Top10IndividualEvent/>}
+          
         </Container>
       </Container>      
     );
