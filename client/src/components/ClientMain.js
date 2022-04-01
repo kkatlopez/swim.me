f9ba749e8eb766f46d299b12d2745d597572815c
Matthew Youngbar <matthewyoungbar@gmail.com>
2022-03-31 20:13:09 -0400

    SD-57: User login endpoints working

--- client/src/components/ClientMain.js
+++ client/src/components/ClientMain.js
@@ -5,6 +5,7 @@ import MeetResults from './MeetResults.js';
 import RosterPage from './Roster.js';
 import SpecificMeet from './SpecificMeet.js'
 import AllTimeTop10 from './AllTimeTop10.js';
+import UserLanding from './UserLogin.js';
 
 // localhost:3000
 
@@ -13,6 +14,9 @@ const Main = (props) => {
   return (
     <Switch>
       <Route exact path='/' render={
+        () => <div><UserLanding/></div>
+      }></Route>
+      <Route exact path='/results' render={
         () => <div><MeetResults/></div>
       }></Route>
       <Route exact path='/meet' render={
@@ -23,7 +27,7 @@ const Main = (props) => {
         () => <div><AllTimeTop10/></div>
       }></Route>
     </Switch>
-    
+
   );
 }
 
