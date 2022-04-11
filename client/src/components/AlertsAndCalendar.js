146a3ddac3a2288ac4fb7d6b67be052fb0440fe1
Gwyneth Yuen <gwyneth.yuen@gmail.com>
2022-04-10 23:48:14 -0400

    SD-67 fix alert order again hopefully

--- client/src/components/AlertsAndCalendar.js
+++ client/src/components/AlertsAndCalendar.js
@@ -46,27 +46,30 @@ class AlertsAndCalendar extends Component {
               return 1;
             }
             if (y.alert_type === "High") {
-              return -1;
+              return 1;
             }
             if (x.alert_type === "High") {
-              return 1;
+              return -1;
             }
             if (y.alert_type === "Medium") {
-              return -1;
+              return 1;
             }
             if (x.alert_type === "Medium") {
-              return 1;
+              return -1;
             }
             if (y.alert_type === "Low") {
-              return -1;
-            }
-            if (x.alert_type === "Low") {
               return 1;
             }
-            if (y.alert_type === "Info") {
+            if (x.alert_type === "Low") {
               return -1;
             }
-            return 1;
+            if (y.alert_type === "Info") {
+              return 1;
+            }
+            if (x.alert_type === "Info") {
+            return -1;
+            }
+            return -1;
           });
 
           const today = new Date().toISOString().split('T')[0];
